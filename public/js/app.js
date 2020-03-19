04346bcaa4117f11ff529cc186d47a9b9669d986
Aditya Pradhan <adipra99@gmail.com>
2020-03-19 19:12:15 +0530

    minor commit

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,7 @@ const weatherForm=document.querySelector('form')
 const search=document.querySelector('input')
 const message1=document.querySelector('#message1')
 const message2=document.querySelector('#message2')
-
+ 
 weatherForm.addEventListener('submit',(e)=>{
     e.preventDefault()
     message1.textContent='Loading....'
